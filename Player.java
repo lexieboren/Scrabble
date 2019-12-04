da892d1093a2d5693ba8d5601c7289148b93b870
lexieboren <58092259+lexieboren@users.noreply.github.com>
2019-12-04 10:02:54 -0800

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Player implements ScrabbleAI {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void setGateKeeper(GateKeeper gateKeeper) {
+
+    }
+
+    @Override
+    public ScrabbleMove chooseMove() {
+        return null;
+    }
+}
